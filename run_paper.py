f396b0b09d9dc2870f8e3cbfe90e7e0dba0b4f61
Edu01062 <edu01062@hotmail.com>
2024-07-08 14:00:12 -0500

    #

--- run_paper.py
+++ run_paper.py
@@ -1,42 +1,52 @@
-
-from subprocess import call
+import os
 import platform
+import shutil
+from subprocess import call
+import streamlit as st
+import glob
 
 system = platform.system()
-
-person = ''     # use the person variable to keep paths in order with multiple machines being used
+person = ''  # Use the person variable to keep paths in order with multiple machines being used
 
 if person == '':
-	path = '/path/to/main_paper'
+    path = '/path/to/main_paper'  # Update this to your actual path
+
+st.title("Process Automation")
 
 if system == 'Windows':
-	pass
-
+    st.write("The script is not configured for Windows.")
 else:
-    print "Cleans Output and Temporary"
-    for folder in ['/output', '/tmp']:
-        shutil.rmtree(path+folder)
-        os.mkdir(path+folder)
-        
-    print "//-- Gets Input --//"
-    call(['python', path+'/code/get_input.py'])
+    if st.button("Clean Output and Temporary"):
+        st.write("Cleaning Output and Temporary")
+        for folder in ['/output', '/tmp']:
+            folder_path = os.path.join(path, folder.strip('/'))
+            if os.path.exists(folder_path):
+                shutil.rmtree(folder_path)
+            os.mkdir(folder_path)
+        st.write("Output and Temporary Cleaned")
 
-    print "//-- Runs Build --//"
-    call(['stata', '-b', 'do' + '\"' + path+'/code/build.do'+'\" &'])
-    for file in glob.glob(path+'/*.log'):
-        os.remove(file)
-    
-    print "//-- Runs Analysis --//"
-    call(['stata', '-b', 'do' + '\"' + path+'/code/analysis.do'+'\" &'])
-    for file in glob.glob(path+'/*.log'):
-        os.remove(file)
-    
-	print "//-- Compiles TeX --//"
-	call(['latexmk', path+'/products/paper/main_article.tex')
+    if st.button("Get Input"):
+        st.write("Getting Input")
+        call(['python', os.path.join(path, 'code/get_input.py')])
+        st.write("Input Obtained")
 
-	print "Congratulations, you have a shiny new paper!"
+    if st.button("Run Build"):
+        st.write("Running Build")
+        call(['stata', '-b', 'do', os.path.join(path, 'code/build.do')])
+        for file in glob.glob(os.path.join(path, '*.log')):
+            os.remove(file)
+        st.write("Build Executed")
 
+    if st.button("Run Analysis"):
+        st.write("Running Analysis")
+        call(['stata', '-b', 'do', os.path.join(path, 'code/analysis.do')])
+        for file in glob.glob(os.path.join(path, '*.log')):
+            os.remove(file)
+        st.write("Analysis Executed")
 
+    if st.button("Compile TeX"):
+        st.write("Compiling TeX")
+        call(['latexmk', os.path.join(path, 'products/paper/main_article.tex')])
+        st.write("TeX Compiled")
 
-
-
+    st.write("Congratulations, you have a shiny new paper!")
